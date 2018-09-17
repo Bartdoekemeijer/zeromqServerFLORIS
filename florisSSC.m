554fb1290feea4dc9353344b84cb518b6b990096
Bartdoekemeijer <B.M.Doekemeijer@tudelft.nl>
2018-09-17 17:04:48 +0200

    Bug fixes and clean up code

--- florisSSC.m
+++ florisSSC.m
@@ -16,30 +16,42 @@
 %
 
 % Setup zeroMQ server
-zmqServer = zeromqObj('/home/bmdoekemeijer/OpenFOAM/zeroMQ/jeromq-0.4.4-SNAPSHOT.jar',5553,300,true);
+zmqServer = zeromqObj('/home/bmdoekemeijer/OpenFOAM/zeroMQ/jeromq-0.4.4-SNAPSHOT.jar',5553,3600,true);
 
-% Add FLORIS path and setup model
+% Add FLORIS path and setup layout
 addpath(genpath('FLORISSE_M/FLORISSE_M'))
 turbines = struct('turbineType', nrel5mw() , ...
-                      'locIf', {[921.5, 1141.3]; [0877.6 1390.3]; [0833.7 1639.2]; ...
-                                [1543.9 1251.0]; [1500.0 1500.0]; [1456.1 1749.0];...
-                                [2166.3 1360.8]; [2122.4 1609.7]; [2078.5 1858.7]});
-layout = layout_class(turbines, 'fitting_9turb'); 
-layout.ambientInflow = ambient_inflow_log('WS', 8,'HH', 90.0,'WD', 0,'TI0', .06);
+                      'locIf', {[868.0  1120.8]; [868.0  1500.0]; [868.0  1879.2]; ...
+                                [1500.0 1120.8]; [1500.0 1500.0]; [1500.0 1879.2];...
+                                [2132.0 1120.8]; [2132.0 1500.0]; [2132.0 1879.2]});
+layout = layout_class(turbines, 'sowfa_9turb'); 
+
+% Purposely initialize with poor initial conditions (to assess estimation)
+%layout.ambientInflow = ambient_inflow_log('WS', 8.0,'HH', 90.0,'WD', 0,'TI0', .06);
+layout.ambientInflow = ambient_inflow_log('WS', 6.5,'HH', 90.0,'WD', deg2rad(10.0),'TI0', .01);
 controlSet = control_set(layout, 'pitch');
 subModels = model_definition('','rans','','selfSimilar','','quadraticRotorVelocity','', 'crespoHernandez');                    
 florisRunner = floris(layout, controlSet, subModels);
 
+% Set FLORIS model parameters to the values found by offline calibration
+xopt = [-0.001338132885368   3.160208854425835  -0.002675041439218  0.327610240042950   0.174472079310193   0.000968572145479]; % From constrained optimization: fitFLORIS.m  
+florisOpt.model.modelData.ad    = xopt(1);
+florisOpt.model.modelData.alpha = xopt(2);
+florisOpt.model.modelData.bd    = xopt(3);
+florisOpt.model.modelData.beta  = xopt(4);
+florisOpt.model.modelData.ka	= xopt(5);
+florisOpt.model.modelData.kb    = xopt(6);
+
 % Initial control settings
 yawAngleArrayOut   = 270.0*ones(1,layout.nTurbs);
 pitchAngleArrayOut = 0.0*ones(1,layout.nTurbs);
-controlTimeInterval  = 10; % Time to wait after applying control signal
-measurementSignalAvgTime = 5; % Time to average data. Needs to be >= controlTimeInterval
+controlTimeInterval  = 600; % Time to wait after applying control signal
+measurementSignalAvgTime = 300; % Time to average data. Needs to be >= controlTimeInterval
 sigma_WD = deg2rad(6)  % 6 deg for 8 m/s by M. Bertele, WES
 dataSend = setupZmqSignal(yawAngleArrayOut,pitchAngleArrayOut);
 
 disp(['Entering wind farm controller loop...']);
-timeLastControl = 0; % -Inf: optimize right away. 0: optimize after controlTimeInterval (no prec), 20e3: optimize after controlTimeInterval (with prec)
+timeLastControl = 20e3; % -Inf: optimize right away. 0: optimize after controlTimeInterval (no prec), 20e3: optimize after controlTimeInterval (with prec)
 measurementVector = [];
 while 1
     % Receive information from SOWFA
@@ -59,27 +71,32 @@ while 1
 			measurementSet = struct();
 			measurementSet.P = struct('values',avgMeasurementVector(1:3:end),'stdev',[1 1 1 2 2 2 3 3 3]);
 			measurementSet.estimParams = {'TI0','Vref'}
-			disp(measurementSet.P)
+			disp(measurementSet.P.values)
 			
 			disp([datestr(rem(now,1)) '__    Doing estimation cycle.']);
-			
 			florisRunner.clearOutput;
+			
+			% Update WD first
+			disp('WD:')
+			WD_measurements = 0.+sigma_WD*randn(1,9) % U=8m/s -> std = 6 deg. according to https://www.wind-energ-sci.net/2/615/2017/wes-2-615-2017.pdf
+			florisRunner.layout.ambientInflow.windDirection = mean(WD_measurements);
+			disp(mean(WD_measurements))
+			
+			% Update WS and TI secondly
 			estTool = estimator({florisRunner},{measurementSet});
 			xopt = estTool.gaEstimation([0.0 6.0],[0.40 10.0]) % Estimate
-			
-			WD_measurements = 0.+sigma_WD*randn(1,9); % U=8m/s -> std = 6 deg. according to https://www.wind-energ-sci.net/2/615/2017/wes-2-615-2017.pdf
-			florisRunner.layout.ambientInflow.windDirection = mean(WD_measurements);
 			florisRunner.layout.ambientInflow.TI0  = xopt(1);
 			florisRunner.layout.ambientInflow.Vref = xopt(2);
-			clear xopt		
+			clear xopt
         end
         
 
         % Optimization
+		WD_uncertainty = sigma_WD/sqrt(length(WD_measurements)); % Uncertainty decreases with sqrt(N)
         disp([datestr(rem(now,1)) '__    Doing optimization cycle.']);
-        [xopt,P_bl,P_opt] = optimizeControlSettingsRobust(florisRunner,'yawOpt',true,'pitchOpt',false,'axOpt',false,sigma_WD/3,5,true);
-        yawAngleArray   = florisRunner.controlSet.yawAngleArray;
-        pitchAngleArray = florisRunner.controlSet.pitchAngleArray;
+        [xopt,P_bl,P_opt] = optimizeControlSettingsRobust(florisRunner,'yawOpt',true,'pitchOpt',true,'axOpt',false,WD_uncertainty,5,true)
+        yawAngleArray   = florisRunner.controlSet.yawAngleArray
+        pitchAngleArray = florisRunner.controlSet.pitchAngleArray
                 
         % Update message string
         yawAngleArrayOut   = round(270.-rad2deg(yawAngleArray),1);
